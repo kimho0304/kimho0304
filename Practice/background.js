757b92cae103338a14a4d37e46db8e9a287e9eb9
kimho0304 <105331004+kimho0304@users.noreply.github.com>
2022-05-22 19:15:04 +0900

    Ver. 1

--- Practice/background.js
+++ Practice/background.js
@@ -1,4 +1,3 @@
-let cnt = 1
 /*
 function Fish(){
     let fish = document.getElementById('fish1')
@@ -49,12 +48,11 @@ function Fish(){
 class Fish{
     constructor(id){
         this.fish = document.getElementById(id)
-        this.stop_ = null
+        this.animate = null
         this.dx = 0
         this.dr = 0
         this.screen_width = window.innerWidth
         this.screen_height = window.innerHeight
-
         this.flag = true
     }
 
@@ -71,6 +69,8 @@ class Fish{
             this.flag = false
         }
 
+        console.log(this.fish.style.top)
+
         if(this.fish.style.top == String(this.screen_height - this.size)+"px"){
             this.init()
             this.fish.style.top = "0px"
@@ -81,27 +81,79 @@ class Fish{
         this.fish.style.transform = "rotate("+String(this.dr)+"deg)"
         this.dr += 0.5
         this.dx += 1
-        this.stop_ = setTimeout(this.move, 5)
+        console.log(this.fish.style.top)
+        this.animate = setTimeout(this.move, 5)
     }
 
     stop(){
-        clearTimeout(this.stop_)
+        clearTimeout(this.animate)
         this.fish.style.top = '0px'
     }
-    
+
 }
+
+function Fish2(tmp_){
+    let fish = document.getElementById(tmp_)
+    let stop_
+    let dx = 0
+    let dr = 0
+    let screen_width = window.innerWidth
+    let screen_height = window.innerHeight
+    let size
+
+    function init2(){
+        size = Math.floor(Math.random()*100)
+        var x = Math.floor(Math.random()*screen_width - size)
+        fish.style.width = String(size)+"px"
+        fish.style.left = String(x)+"px"
+    }
+
+    flag = true
+
+    function move2(){
+        console.log(screen_height)
+        if(flag){
+            init2()
+            flag = false
+        }
+
+        if(fish.style.top == String(screen_height - size - 100)+"px"){
+            init2()
+            fish.style.top = "0px"
+            dx = 0
+        }
+
+        fish.style.top = String(dx+1)+"px";
+        fish.style.transform = "rotate("+String(dr)+"deg)"
+        dr += 0.5
+        dx += 1
+        stop_ = setTimeout(move2, 5)
+    }
+
+    /*function stop2(){
+        clearTimeout(stop_)
+        fish.style.top = '0px'
+    }*/
+
+    move2()
+}
+
+let cnt = 1
 
 function create(){
     let area = document.getElementById('items')
     let item = document.createElement('img')
-    var st = "width: 100px; top: 0px; left: 0px; z-index: -1; position: absolute; "
-    
-    item.setAttribute('src', 'fish.png')
-    item.setAttribute('id', 'fish'+String(cnt))
-    item.style.cssText = st
+    let st = "width: 100px; top: 0px; left: 0px; z-index: -1; position: absolute; "
+    var t = "img/food/"
+    let img_list = [t+'fish.png', t+'bread.png', t+'cookie.png', t+'chicken.png', t+'steak.png', t+'salmon.png', t+'melon.png']
 
+    item.setAttribute('src', img_list[Math.floor(Math.random()*img_list.length)])
+    item.setAttribute('id', 'item'+String(cnt))
+    item.style.cssText = st
     area.appendChild(item)
-    var fuck = new Fish('fish'+String(cnt))
-    fuck.move()
+    
+    //let tmp = new Fish('fish'+String(cnt))
+    Fish2('item'+String(cnt))
     cnt+=1
+    
 }
--- Practice/centre.js
+++ Practice/centre.js
@@ -21,16 +21,17 @@ function tmp(instance){
 
 
 //centre 첫 번째 div
-p = []
-for(var i = 0; i < 4; i++){
+var p = []
+var C = 5
+for(var i = 0; i < C; i++){
     p[i] = new Image()
-    p[i].src = "img/img"+(i+1)+".png"
+    p[i].src = "img/looks/img"+(i+1)+".png"
 }
 
 inx = 0
 
 function img_change(k){
-    inx = ((inx+k)+4) % 4
+    inx = ((inx+k)+C) % C
     document.getElementById('picture').setAttribute('src', p[inx].src)
     //console.log(p[inx])
 }
@@ -44,5 +45,45 @@ let centre2 = document.getElementById("centre2");
 tmp(centre2)
 
 //centre 세 번째 div
-let centre3 = document.getElementById("centre3");
-tmp(centre3)
+//let centre3 = document.getElementById("centre3");
+//tmp(centre3)
+
+
+function chg_img(event){
+  let img_path = 'img/albums/'
+  let img_type = '.jpg'
+  let mp3_path = 'mp3/'
+  let mp3_type = '.mp3'
+
+  let mp3_album = {
+    'graduation':'Stronger',
+    'ye' : 'Ghost Town',
+    'yeezus' : 'Black Skinhead',
+    '808s&heartbreak' : 'Heartless',
+    'donda' : 'Jail',
+    'mbdtf' : 'All Of The Lights',
+    'jesusisking' : 'Use This Gospel',
+    'thelifeofpablo' : 'No More Parties in LA'
+  }
+  
+  let mp3_img = document.getElementById('mp3_img') //앨범커버
+  let mp3 = document.getElementById('mp3') //iframe
+  let mp3_title = document.getElementById('mp3_title') //노래제목
+
+  var fuckstring = "http://127.0.0.1:5500/"
+  mp3_img.src = event.target.src.replace(fuckstring, '') //이미지주소
+  console.log(event.target.src.replace(fuckstring, ''))
+  console.log(mp3_img.src)
+
+  var tmp_ = event.target.src.replace(img_path, '').replace(img_type, '').replace(fuckstring, '')
+  console.log(tmp_)
+
+  var mp3_ = (mp3_path + mp3_album[tmp_] + mp3_type)
+  mp3.src = mp3_.replace(fuckstring, '')
+  console.log(mp3.src, mp3_)
+  mp3_title.innerHTML = mp3_album[tmp_]
+
+  mp3_img.style.cssText = "width: 200px;  height: 200px;"
+}
+
+
